53ee3f4ea8618972171954223c9e217bfb0542c0
Vasanth <144214871+surfvasanth@users.noreply.github.com>
2023-09-10 11:48:20 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import "./styles.css";
 
 const App = () => {
   const name='vasanth';
-  const isNameShowing=false;
+  
   return (
     <div className="App">
-      <h1>React {isNameShowing ? name : 'Ramesh'}</h1>
+      <h1>React </h1>
+      {name ?
+      (<>test</>):
+      (<h1>No</h1>)}
      </div>
   );
 }
