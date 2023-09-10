3d2668928adb05906fe9dc7bf0b5a278ec4005f8
Vasanth <144214871+surfvasanth@users.noreply.github.com>
2023-09-10 11:49:56 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ const App = () => {
       <h1>React </h1>
       {name ?
       (<>test</>):
-      (<h1>No</h1>)}
+      (<><h1>No</h1>
+      <h1>There is no name</h1>
+      </>)}
      </div>
   );
 }
